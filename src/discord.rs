90d0823f17b4d081fdf54e140701bb05fec825fe
VilotStar <vilotstarbiss@gmail.com>
2022-01-01 06:09:38 +0000

    Base of multithreading

--- src/discord.rs
+++ src/discord.rs
@@ -1,9 +1,13 @@
 #![allow(non_snake_case)]
 
+use json::JsonValue;
+
 use crate::download;
 
 pub async fn getImages(chanID: String, amount: i32, token: &str) -> Result<(), reqwest::Error> {
     let client = reqwest::Client::new();
+    let mut queue: Vec<JsonValue> = Vec::new();
+
     if amount <= 100 {
         let url = format!("https://canary.discord.com/api/v9/channels/{}/messages?limit=100", chanID);
         let res = client
@@ -13,7 +17,8 @@ pub async fn getImages(chanID: String, amount: i32, token: &str) -> Result<(), r
             .await.unwrap();
 
         let data = json::parse(&res.text().await.unwrap()).unwrap();
-        download::parse_json(&data).await;
+        queue.push(data.clone());
+        //download::parse_json(&data).await;
     } else {
         let url = format!("https://canary.discord.com/api/v9/channels/{}/messages?limit=100", chanID);
         let res = client
@@ -23,6 +28,8 @@ pub async fn getImages(chanID: String, amount: i32, token: &str) -> Result<(), r
             .await.unwrap();
 
         let data = json::parse(&res.text().await.unwrap()).unwrap();
+        //download::parse_json(&data).await;
+        queue.push(data.clone());
 
         let mut lastMsg = data.members().last().unwrap()["id"].clone();
         let amountHund = f64::from(amount / 100).floor() - 1_f64;
@@ -37,8 +44,8 @@ pub async fn getImages(chanID: String, amount: i32, token: &str) -> Result<(), r
                 .await.unwrap();
 
             let dataHund = json::parse(&res.text().await.unwrap()).unwrap();
-
-            download::parse_json(&dataHund).await;
+            queue.push(dataHund.clone());
+            //download::parse_json(&dataHund).await;
 
             lastMsg = data.members().last().unwrap()["id"].clone();
         }
@@ -51,9 +58,11 @@ pub async fn getImages(chanID: String, amount: i32, token: &str) -> Result<(), r
             .await.unwrap();
 
         let dataTens = json::parse(&res.text().await.unwrap()).unwrap();
-
-        download::parse_json(&dataTens).await;
+        queue.push(dataTens.clone());
+        //download::parse_json(&dataTens).await;
     }
+
+    download::multithread_queue(queue).await;
 
     Ok(())
 }
--- src/download.rs
+++ src/download.rs
@@ -2,6 +2,28 @@
 
 use std::path::Path;
 use json::JsonValue;
+use std::sync::mpsc;
+use std::thread;
+use std::mem;
+
+pub async fn multithread_queue(queue: Vec<JsonValue>) {
+    let (tx, rx) = mpsc::channel();
+    for i in 0..queue.len() {
+        let tx = tx.clone();
+        let data = queue[i].clone();
+        thread::spawn(move || { // Find a way to use/move the 2 functions in a thread to be used
+            //parse_json(&data).await;
+            
+            tx.send(i).unwrap(); // Add some type of error check and send to main thread
+        });
+    }
+
+    mem::drop(tx);
+
+    for r in rx {
+        println!("{} finished task.", r);
+    } // In case of error from thread, kill it/join it, then make new thread for that task (might be lazy and just kill all threads then try agian)
+}
 
 pub async fn parse_json(json: &JsonValue) {
     for message in json.members() {
